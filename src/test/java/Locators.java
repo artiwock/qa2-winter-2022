f613d94d6504a573270be0e918a60aeed3fcd9fa
Artjom B <test@test.lv>
2022-11-06 12:39:30 +0200

    HW3 - Locators

---
+++ src/test/java/Locators.java
@@ -0,0 +1,41 @@
+import org.openqa.selenium.By;
+
+public class Locators {
+    // .//div[@class = 'logo']
+    // .//li[@class = 'navigation-item'][1]
+    // .//a[contains(@class, 'link action my')]
+    // .//div[contains(@class, 'caption') or .//div[(@class = 'home-caption')]
+    // .//div[@class = 'fake-field clearfix']
+    // .//span[@class = 'sb-residence-text'] or .//span[contains(@class, 'sb-residence')]
+    // .//button[contains(@class, 'green button')]
+    // .//div[@id = 'review-arrow-left']
+    // .//div[@class = 'tp-widget-review']
+    // .//div[@class ='row is-table-row']
+
+    /////////////////////////////////////////////////////////////////
+
+    // .//div[contains(@class, 'car-top-filter-item')][1]
+    // .//div[contains(@class, 'control next')]
+    // .//span[contains(@class, 'showing')]
+    // .//a[contains(@class, 'dc')]
+    // .//li[@class = '']
+    // .//div[(@class = 'flt-type')][1]
+    // .//a[@class = 'open-modal search-index trigger-gtm-rc rental-conditions with-logo']
+    // .//div[@class = 'ca-table-cell top'][1]
+    // .//span[@class='min-price']
+    // .//label[@data-event-label='Gasoline']
+    // .//span[@class='dc-ui badge badge-medium badge-blue']
+    // .//label[@for='number-of-seats-seats-4']
+    // .//div[@class = 'supplier-rating']
+    // .//div[@class = 'price-item-price-main']
+
+    //////////////////////////////////////////////////////////////
+
+    // .//div[@class = 'lds-ellipsis v2']
+    // .//div[contains(@class, 'mb-8')]
+    // .//div[contains(@class, 'mb-24')]
+
+
+
+
+}
